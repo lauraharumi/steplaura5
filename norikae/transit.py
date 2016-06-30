0d4c6483ffeff772e5c0cdb4c282d2ccfa7a7dea
lauraharumi <laura.vazquez.fukazawa@gmail.com>
2016-06-30 19:29:57 +0900

    Update transit.py

--- norikae/transit.py
+++ norikae/transit.py
@@ -6,13 +6,10 @@ import time
 import json
 
 localtime = time.asctime( time.localtime(time.time()) )
-
 file = open("main.html","r")
 html = file.read()
-
 url = open("data.json","r")
 output = json.load(url)
-
 outrages = open("outrages.json","r")
 out_data = json.load(outrages)
 
@@ -26,13 +23,13 @@ def create_graph(output):
             for j in (i-1, i+1):
                 if 0 <= j < len(eki_list):
                     graph[eki].append(eki_list[j])
-    return graph 
-
+    return graph
+    
 def outrages(graph, out_data):
     for from_to in out_data:
         graph[from_to['From']].remove(from_to['To'])
     return graph
-
+    
 def find_route(graph, start, end, route=[]):
     route = route + [start]
     if start == end:
@@ -51,17 +48,14 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(localtime + html)
         start, end = self.request.get("from"), self.request.get("to")
         out_checked = self.request.get("check")
-
         self.response.write("<h4> From: " + start + " To: "+ end + out_checked + "</h4><br>")
-        
         if out_checked: 
             route = find_route(outrages(create_graph(output), out_data), start, end)
         else: 
             route = find_route(create_graph(output), start, end)
-
         for station in route: 
             self.response.write(station + " --> ")
-
+            
 app = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
